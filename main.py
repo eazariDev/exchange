23c62c5a71f82011efa63fac18ee058cb64626d8
eazaridev <eazari.dev@gmail.com>
2023-07-12 10:50:36 -0400

    add email report feature

--- main.py
+++ main.py
@@ -2,8 +2,13 @@
 
 import requests
 import json
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from config import url, rules, senderEmail, appPassword, receivers
 
-from config import url
+
+
 
 def get_rates():
     response = requests.get(url)
@@ -12,15 +17,47 @@ def get_rates():
     return None
 
 
+
 def archive(filename, rates):
     with open("archive/"+filename+".json", 'w') as f:
         f.write(json.dumps(rates))
 
 
+
+def mail_service(timestamp, rates):
+    subject = "Currency rates at " + timestamp
+    if rules["preferred"] is not None:
+        tmp = dict()
+        for currency in rules['preferred']:
+            tmp[currency] = rates[currency]
+        rates = tmp
+    body = json.dumps(rates)
+    for r in receivers:
+        send_email(r, subject, body)
+
+
+def send_email(toAddress, subject, body):
+
+    message = MIMEMultipart()
+    message["To"] = toAddress
+    message["From"] = senderEmail
+    message["Subject"] = subject
+    title = "<b>"+subject+" <b>"
+    message.attach(MIMEText(body))
+    server = smtplib.SMTP("smtp.gmail.com:587")
+    server.ehlo("Gmail")
+    server.starttls()
+    server.login(senderEmail, appPassword)
+    server.sendmail(senderEmail, toAddress, message.as_string())
+    server.quit()
+
+
 if __name__ == '__main__':
     response = get_rates()
-    archive(str(response['timestamp']), response['rates'])
-
+    if rules['archive']:
+        archive(str(response['timestamp']), response['rates'])
+    if rules['mail']:
+        mail_service(str(response['timestamp']), response['rates'])
 
 
 

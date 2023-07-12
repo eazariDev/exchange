bde0a6083dd2c683f6700b1058185bc10046c2df
eazaridev <eazari.dev@gmail.com>
2023-07-12 11:00:25 -0400

    final changes

--- config.py
+++ config.py
@@ -1,5 +1,16 @@
 BASE_URL = 'http://data.fixer.io/api/latest?access_key='
-API_KEY = '9514335fa827f37db90e2e9a469e5ac4'
-
+API_KEY = 'YOUR_API_KEY'
 url = BASE_URL + API_KEY
+
+senderEmail = 'YOUR_EMAIL_ADDRESS'
+appPassword = 'APPLICATION_PASSWORD'
+receivers = ['YOUR_RECEIVERS']
+
+rules ={ 
+        'archive': True,
+        'mail' : True,
+        #defualt, meaning all of currency rates
+        #'preferred': None
+        'preferred': ['BTC', 'IRR']
+        }
 
